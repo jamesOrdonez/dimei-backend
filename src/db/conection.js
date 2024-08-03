2186ce02b066430e3c396fd7c4dea5dfc594b8b2
JamesOrdonez <jamesordonez308@gmail.com>
2024-08-03 12:28:28 -0500

    add modules proyect

--- src/db/conection.js
+++ src/db/conection.js
@@ -2,10 +2,18 @@
 
 const mysql = require("mysql2/promise");
 
-const connection = mysql.createPool({
+/* const connection = mysql.createPool({
   host: "localhost",
   user: "whoaomi",
   password: "12345678",
+  database: "dimei",
+  port: 3306,
+}); */
+
+const connection = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
   database: "dimei",
   port: 3306,
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,9 @@ const Product = require("./v1/product.routes");
 const Module = require("./v1/module.routes");
 const Permission = require("./v1/permission.routes");
 const Item_Product = require("./v1/item_product.routes");
-const Company = require("./v1/company.routes")
+const Company = require("./v1/company.routes");
+const Equipmenttype = require("./v1/equipmentType.routes");
+const Proyect = require("./v1/proyect.routes");
 
 routes.use(User);
 routes.use(Login);
@@ -20,5 +22,7 @@ routes.use(Module);
 routes.use(Permission);
 routes.use(Item_Product);
 routes.use(Company);
+routes.use(Equipmenttype);
+routes.use(Proyect);
 
 module.exports = routes;
