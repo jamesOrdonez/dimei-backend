3a57c8e36f94e88a17bd429eb56ab5ddbd6e0735
James Santiago Ordoñez <83985299+jamesOrdonez@users.noreply.github.com>
2025-11-25 20:58:23 -0500

    Refactor server.js for CORS and middleware setup

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
-//! API documentation for the following methods
 const express = require("express");
-const body = require("body-parser");
 const cors = require("cors");
 
 const app = express();
@@ -8,25 +6,21 @@ const PORT = 8080;
 
 const v1Routes = require("./v1/routes");
 
-app.use(cors());
-app.use(express.json());
-app.use(body.json({ limit: "50mb", extended: true }));
-app.use(body.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.json());
+// Middleware
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// Configuración CORS 
+app.use(cors({
+  origin: "http://vps.equiposdimei.com",
+  methods: ["GET","POST","PUT","DELETE","PATCH"],
+  credentials: true // si envías cookies
+}));
 
+// Rutas
 app.use("/api/v1/", v1Routes);
 
+// Servidor
 app.listen(PORT, () => {
-  console.warn(`server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
