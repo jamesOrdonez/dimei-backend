c9d0c044333c5fa3c6be5951d6d51a541c2ad679
whoaomi59 <whoaomi11@gmail.com>
2026-01-09 12:35:07 -0500

    ok-unidadmedida

--- src/controllers/unitOfMeasure.controller.js
+++ src/controllers/unitOfMeasure.controller.js
@@ -1,6 +1,6 @@
 const httpStatus = require("http-status");
 const conection = require("../db/conection");
-const Module = "unitOfMeasuremet";
+const Module = "unitofmeasure";
 
 async function get(req, res) {
   try {
@@ -23,6 +23,79 @@ async function get(req, res) {
   }
 }
 
+async function post(req, res) {
+  try {
+    const { unitOfMeasure, company } = req.body;
+    const save = await conection.execute(
+      `INSERT INTO unitofmeasure (unitOfMeasure, company) VALUES (?, ?)`,
+      [unitOfMeasure, company]
+    );
+    if (save) {
+      res.status(httpStatus.CREATED).json({
+        message: "Registro creado",
+        module: Module,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: `Error interno en el servidor: ${error}`,
+      module: Module,
+    });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const { unitOfMeasure } = req.body;
+    const id = req.params.id;
+
+    const updatePermiss = await conection.execute(
+      `UPDATE unitofmeasure SET unitOfMeasure=? WHERE id = ?`,
+      [unitOfMeasure, id]
+    );
+
+    if (updatePermiss) {
+      res.status(httpStatus.OK).json({
+        message: "Registro actualizado",
+        module: Module,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: `Error interno en el servidor: ${error}`,
+      module: Module,
+    });
+  }
+}
+
+async function Delete(req, res) {
+  try {
+    const id = req.params.id;
+
+    const deleteProyect = await conection.execute(
+      `DELETE FROM ${Module} WHERE id = ?`,
+      [id]
+    );
+    if (deleteProyect) {
+      res.status(httpStatus.OK).json({
+        message: "Registro eliminado",
+        module: Module,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: `Error interno en el servidor: ${error}`,
+      module: Module,
+    });
+  }
+}
+
 module.exports = {
   get,
+  post,
+  update,
+  Delete,
 };
--- src/routes/v1/unitOfMeasuremet.routes.js
+++ src/routes/v1/unitOfMeasuremet.routes.js
@@ -3,5 +3,8 @@ const controller = require("../../controllers/unitOfMeasure.controller");
 const Module = "unitOfMeasuremet";
 
 router.get("/unitOfMeasuremet", controller.get);
+router.post("/unitOfMeasuremet", controller.post);
+router.put("/unitOfMeasuremet/:id", controller.update);
+router.delete("/unitOfMeasuremet/:id", controller.Delete);
 
 module.exports = router;
