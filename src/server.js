6619e3a85833d803dcfecb94da9fd421c67deaa5
whoaomi59 <whoaomi11@gmail.com>
2026-01-31 17:06:30 -0500

    redireccion qr

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     /*  origin: "https://vps.equiposdimei.com", */
 
-    origin: "http://192.168.1.104:3000",
+    origin: "http://192.168.0.101:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // si envías cookies
   }),
