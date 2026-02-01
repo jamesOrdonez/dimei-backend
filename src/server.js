a03f6b9316fa655331c07c76ade35cb6d3a1e7b8
whoaomi59 <whoaomi11@gmail.com>
2026-02-01 15:42:08 -0500

    filtro producto

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     /*  origin: "https://vps.equiposdimei.com", */
 
-    origin: "http://localhost:3000",
+    origin: "http://192.168.0.101:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // si envías cookies
   }),
