324c4de45c86b3784d4c382f423ee807018f2c34
JamesOrdonez <jamesordonez308@gmail.com>
2026-02-01 21:51:46 -0500

    cambios produccion

--- src/server.js
+++ src/server.js
@@ -14,9 +14,8 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Configuración CORS
 app.use(
   cors({
-    /*  origin: "https://vps.equiposdimei.com", */
-
-    origin: "http://192.168.0.101:3000",
+    origin: "https://vps.equiposdimei.com",
+    /*     origin: "http://localhost:3000", */
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // si envías cookies
   }),
